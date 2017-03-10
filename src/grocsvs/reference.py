ab51dbcfed7ff516213216a0d2f369aec700b6e5
Noah Spies <nspies@eniac.local>
2017-03-10 09:49:45 -0800

    fix when using ensembl-format chromosome numbering (ie 1, 2, 3,...)

--- src/grocsvs/reference.py
+++ src/grocsvs/reference.py
@@ -19,15 +19,15 @@ class Reference(object):
             std_chrom = self.standardize_chrom(chrom)
             if re.match(chrom_re, std_chrom):
                 self.chroms_to_std_chroms[chrom] = std_chrom
-                self.std_chroms_to_chroms[chrom] = chrom
-                self.chrom_lengths[std_chrom] = len(self.fasta[chrom])
+                self.std_chroms_to_chroms[std_chrom] = chrom
+                self.chrom_lengths[chrom] = len(self.fasta[chrom])
 
     @property
     def chroms(self):
         if self.debug:
             return ["chr20", "chr21"]
 
-        return self.std_chroms_to_chroms.keys()
+        return self.chrom_lengths.keys()
 
     def standardize_chrom(self, chrom):
         if "chr" in chrom:
