f1561d525811c1a6c94ef0dc81b892701ee67d0f
nspies <nspies@stanford.edu>
2017-05-02 11:07:24 -0700

    minor refactor of pipelining code

--- src/grocsvs/pipeline.py
+++ src/grocsvs/pipeline.py
@@ -22,43 +22,55 @@ class Runner(object):
         self.jobmanagers = None
 
 
-    def run_stage(self, stage, stage_name):
+    def run_stage(self, stage, stage_name, restart=0):
         to_run = []
-        all_steps = stage.get_steps(self.options)
+        all_steps = list(stage.get_steps(self.options))
         for step_chunk in all_steps:
             if step_chunk.needs_to_run():
                 to_run.append(step_chunk)
-                
-        # should probably have an option to re-use jobmanagers started
-        # earlier
 
         logging.info("{} {} {}".format("="*30, stage_name, "="*30))
 
         if len(to_run) > 0:
             logging.info("{} chunks to run. Starting...".format(len(to_run)))
 
-            utilities.ensure_dir(to_run[0].results_dir)
-            utilities.ensure_dir(to_run[0].working_dir)
-            utilities.ensure_dir(os.path.join(self.options.log_dir, to_run[0].__class__.__name__))
+            self.ensure_dirs(to_run)
+            self.launch(to_run, all_steps, stage, stage_name, restart)
+            
+            logging.info("--> {} completed.\n".format(stage_name))
+        else:
+            logging.info("--> 0 chunks need to be run. Skipping...\n")
 
+
+    def ensure_dirs(self, to_run):
+        utilities.ensure_dir(to_run[0].results_dir)
+        utilities.ensure_dir(to_run[0].working_dir)
+        utilities.ensure_dir(os.path.join(self.options.log_dir, to_run[0].__class__.__name__))
+
+
+    def launch(self, to_run, all_steps, stage, stage_name, restart):
+        try:
             processes = min(self.options.cluster_settings.processes, len(to_run))
             jobmanagers = make_jobmanager(
                 self.options.cluster_settings, processes, self.options.working_dir)
             jobmanagers.map(_run_chunk, to_run)
+        except Exception as e:
+            print "ERROR RUNNING STAGE '{}': {}".format(stage_name, e)
 
-            error_not_done = []
-            for step_chunk in all_steps:
-                if step_chunk.needs_to_run():
-                    error_not_done.append(step_chunk)
-            if len(error_not_done) > 0:
+        error_not_done = []
+        for step_chunk in all_steps:
+            if step_chunk.needs_to_run():
+                error_not_done.append(step_chunk)
+
+        if len(error_not_done) > 0:
+            if restart > 0:
+                self.run_stage(stage, stage_name, restart=restart-1)
+            else:
                 logging.error("Failed to complete the following steps:\n{}".format(
                     "\n".join(step_chunk.log_path for step_chunk in error_not_done)))
                 raise Exception(("Stage {} failed to complete. Check the log files " +
                                  "above for more information.").format(stage_name))
                 
-            logging.info("--> {} completed.\n".format(stage_name))
-        else:
-            logging.info("--> 0 chunks need to be run. Skipping...\n")
 
 
 def _run_chunk(chunk):
