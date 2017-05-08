538f2a042a959dc625623625056f3a5b425334c4
nspies <nspies@stanford.edu>
2017-05-08 11:31:48 -0700

    clarification for genotype field in vcf

--- src/grocsvs/stages/export_vcf.py
+++ src/grocsvs/stages/export_vcf.py
@@ -15,7 +15,7 @@ VCF_HEADER = \
 ##FILTER=<ID=NOLONGFRAGS,Description="No samples found with long fragments supporting event (likely segdup)">
 ##FILTER=<ID=NEARBYSNVS,Description="Very high number of possible SNVs detected in vicinity of breakpoint (likely segdup)">
 ##FILTER=<ID=ASSEMBLYGAP,Description="Nearby gap in the reference assembly found; these gaps can appear to mimic SVs">
-##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype (currently either 0/1 when breakpoint is present in sample or 0/0 if not)"> 
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype (currently either 1 when breakpoint is present in sample or 0 if not)"> 
 ##FORMAT=<ID=BS,Number=1,Type=int,Description="Number of barcodes supporting the event"> 
 ##FORMAT=<ID=BT,Number=1,Type=int,Description="Total number of barcodes, calculated as the union of barcodes at each site"> 
 ##FORMAT=<ID=F50,Number=1,Type=Float,Description="Median fragment length supporting the breakpoint; estimated as the total across all supporting segments"> 
@@ -79,9 +79,9 @@ def get_sample_info(sample, event):
     sample_info["BS"] = event["{}_shared".format(sample.name)]
     sample_info["BT"] = event["{}_total".format(sample.name)]
     if sample_info["BS"] > 5:
-        sample_info["GT"] = "0/1"
+        sample_info["GT"] = "1"
     else:
-        sample_info["GT"] = "0/0"
+        sample_info["GT"] = "0"
 
     if "{}_lengths_50".format(sample.name) in event:
         sample_info["F50"] = event["{}_lengths_50".format(sample.name)]
